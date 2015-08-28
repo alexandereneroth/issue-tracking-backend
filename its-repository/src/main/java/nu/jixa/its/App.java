55b54a57b5a7f4583fe7f5fd85e76fb378424075
Jonatan Ruohomäki <jonaru@gmail.com>
2015-08-28 07:27:10 +0200

    Initial commit with .gitignore

---
+++ its-repository/src/main/java/nu/jixa/its/App.java
@@ -0,0 +1,13 @@
+package nu.jixa.its;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
