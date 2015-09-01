efe6fdb17f854433bd58fe8dcf5ae78e6f29cf48
alexander-eneroth <89alexius@gmail.com>
2015-09-01 10:35:44 +0200

    feat: adds Service config bean and userServiceTest method stubs

--- its-repository/src/main/java/nu/jixa/its/config/ServiceConfig.java
+++ its-repository/src/main/java/nu/jixa/its/config/ServiceConfig.java
@@ -1,8 +1,13 @@
 package nu.jixa.its.config;
 
+import nu.jixa.its.service.ServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfig {
 
+  @Bean ServiceImpl serviceImpl(){
+    return new ServiceImpl();
+  }
 }
--- its-repository/src/test/java/nu/jixa/its/UserServiceTest.java
+++ its-repository/src/test/java/nu/jixa/its/UserServiceTest.java
@@ -2,10 +2,12 @@ package nu.jixa.its;
 
 import nu.jixa.its.config.InfrastructureConfig;
 import nu.jixa.its.config.ServiceConfig;
+import nu.jixa.its.service.Service;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -15,10 +17,14 @@ import static org.junit.Assert.fail;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { InfrastructureConfig.class,
     ServiceConfig.class }, loader = AnnotationConfigContextLoader.class)
-public class UserRepositoryTest {
+public class UserServiceTest {
+
+  @Autowired
+  Service service;
 
   @Before
   public void before() {
+
     fail();
   }
 
@@ -29,6 +35,36 @@ public class UserRepositoryTest {
 
   @Test
   public void canSaveAndRemove() {
+
+  }
+
+  @Test
+  public void canUpdate(){
+    fail();
+  }
+
+  @Test
+  public void canGet(){
+    fail();
+  }
+
+  @Test
+  public void canGetById(){
+    fail();
+  }
+
+  @Test
+  public void canGetByTeam(){
+    fail();
+  }
+
+  @Test
+  public void canGetByNameLike(){
+    fail();
+  }
+
+  @Test
+  public void canAddWorkItemTo(){
     fail();
   }
 }
