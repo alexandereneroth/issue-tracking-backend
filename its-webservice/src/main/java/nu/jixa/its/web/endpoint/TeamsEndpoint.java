15fc7d86bb9d89a1261b4ea3f9a84bac8fa516f6
Jonatan Ruohomäki <jonaru@gmail.com>
2015-09-11 01:44:38 +0200

    Started on TeamsEndpoint

--- its-webservice/src/main/java/nu/jixa/its/web/endpoint/TeamsEndpoint.java
+++ its-webservice/src/main/java/nu/jixa/its/web/endpoint/TeamsEndpoint.java
@@ -2,8 +2,10 @@ package nu.jixa.its.web.endpoint;
 
 import java.net.URI;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -22,6 +24,7 @@ import org.springframework.stereotype.Component;
 public class TeamsEndpoint {
   private static final String BAD_REQUEST_NULL_OR_INVALID =
       "Null or Invalid JSON Data in Request Body";
+  private static final String NO_TEAM_WITH_TEAM_NUMBER = "No team with Team Number: ";
 
   @Autowired
   private ITSRepository itsRepository;
@@ -36,6 +39,17 @@ public class TeamsEndpoint {
   //✓Team       | Hämta alla team
   //✓UserTeam   | Lägga till en User till ett team
 
+  @GET
+  @Path("{teamNumber}")
+  public Response getUser(@PathParam("teamNumber") final long teamNumber) {
+    try {
+      Team team = itsRepository.getTeam(teamNumber);
+      return Response.ok(team).build();
+    } catch (ITSRepositoryException e) {
+      return Response.status(Response.Status.NOT_FOUND)
+          .entity(NO_TEAM_WITH_TEAM_NUMBER + teamNumber).build();
+    }
+  }
 
   //✓Team       | Skapa ett team
   @POST
@@ -55,5 +69,4 @@ public class TeamsEndpoint {
     final URI location = uriInfo.getAbsolutePathBuilder().path(team.getNumber().toString()).build();
     return Response.created(location).build();
   }
-
 }
