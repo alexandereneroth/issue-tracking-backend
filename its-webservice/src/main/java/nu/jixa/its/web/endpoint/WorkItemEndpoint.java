33607a8706b8518d1021a55aa57f5d88e3af512c
alexander-eneroth <89alexius@gmail.com>
2015-09-10 20:25:41 +0200

    (Not Buildable)feat: Started with WorkItemEndpoint

---
+++ its-webservice/src/main/java/nu/jixa/its/web/endpoint/WorkItemEndpoint.java
@@ -0,0 +1,104 @@
+package nu.jixa.its.web.endpoint;
+
+import java.net.URI;
+import java.util.Collection;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import nu.jixa.its.model.Status;
+import nu.jixa.its.model.User;
+import nu.jixa.its.model.WorkItem;
+import nu.jixa.its.service.ITSRepository;
+import nu.jixa.its.service.ITSRepositoryException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class WorkItemEndpoint {
+
+  @Autowired
+  private ITSRepository itsRepository;
+
+  @Context
+  private UriInfo uriInfo;
+
+  private static final String NO_WORKITEM_WITH_NUMBER = "No workItem with number: ";
+  private static final String BAD_REQUEST_NULL_OR_INVALID =
+      "Null or Invalid JSON Data in Request Body";
+  private static final String BAD_REQUEST_MISMATCH_BETWEEN_PATH_AND_USER =
+      "Usernumber mismatch between path and new user info";
+
+  //✓WorkItem   | Skapa en work item
+  @POST
+  public Response createWorkItem(final WorkItem workItem) throws IllegalAccessException {
+
+    if (workItem == null) {
+      return Response.status(Response.Status.BAD_REQUEST)
+          .entity(BAD_REQUEST_NULL_OR_INVALID).build();
+    }
+
+    try {
+      itsRepository.addWorkItem(workItem);
+    } catch (ITSRepositoryException e) {
+      return Response.status(Response.Status.BAD_REQUEST)
+          .entity(BAD_REQUEST_NULL_OR_INVALID).build();
+    }
+
+    final URI location = uriInfo.getAbsolutePathBuilder().path(workItem.getNumber().toString()).build();
+    return Response.created(location).build();
+  }
+  //✓WorkItem   | Ändra status på en work item
+  //✓WorkItem   | Ta bort* en work item
+  @DELETE
+  @Path("{workItemNumber}")
+  public Response deleteWorkItem(@PathParam("workItemNumber") final long workItemNumber) {
+    try {
+      itsRepository.removeWorkItem(workItemNumber);
+    } catch (ITSRepositoryException e) {
+      return Response.status(Response.Status.NOT_FOUND)
+          .entity(NO_WORKITEM_WITH_NUMBER + workItemNumber).build();
+    }
+    return Response.noContent().build();
+  }
+  //✓WorkItem   | Tilldela en work item till en User
+
+  //✓WorkItem (run by itself)  | Söka efter work item som innehåller en viss text i sin beskrivning
+  //✓WorkItem   | Hämta alla work item baserat på status
+  //✓WorkItem   | Hämta alla work item för ett Team
+  //✓WorkItem   | Hämta alla work item för en User
+  //✓WorkItem   | Hämta alla work item som har en Issue
+  private static final String STATUS_IN_PROGRESS = "in_progress";
+  private static final String STATUS_ON_BACKLOG = "on_backlog";
+  private static final String STATUS_DONE = "done";
+  @GET
+  public Response getUsers(
+      @QueryParam("team") @DefaultValue("") final String teamNumber,
+      @QueryParam("user") @DefaultValue("") final String userNumber,
+      @QueryParam("status") @DefaultValue(STATUS_IN_PROGRESS) final String status,
+      @QueryParam("description_contains") @DefaultValue("") final String descriptionSubstring,
+      @QueryParam("has_issue") @DefaultValue("") final String hasIssue) {
+
+    //Collection<User> usersByName = itsRepository.getUsersByNameLike(searchString);
+    //if (usersByName.isEmpty()) {
+    //  return Response.noContent().build();
+    //} else {
+    //  return Response.ok(usersByName).build();
+    //}
+  }
+  //✓WorkItem   | Skapa en Issue
+  //✓WorkItem   | Uppdatera en Issue
+  //✓WorkItem   | Lägga till en Issue till en work item
+}
