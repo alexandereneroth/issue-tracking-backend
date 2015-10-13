64e94f2405f9e63b369f797dfea2a756c884024f
Jonatan Ruohomäki <jonaru@gmail.com>
2015-10-13 14:56:57 +0200

    fix: Fixes rootendpoint and adds passwords to dummydata

--- its-webservice/src/main/java/nu/jixa/its/web/endpoint/RootEndpoint.java
+++ its-webservice/src/main/java/nu/jixa/its/web/endpoint/RootEndpoint.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class RootEndpoint {
--- its-webservice/src/main/resources/import.sql
+++ its-webservice/src/main/resources/import.sql
@@ -4,11 +4,11 @@ SET foreign_key_checks=0;
 
 insert into tblTeam (id, number) values (1, 101);
 
-insert into tblUser (id, number, firstname, lastname, username, team_id) values (1, 101, 'Kelly', 'Lewis', 'klewis0', 1);
-insert into tblUser (id, number, firstname, lastname, username, team_id) values (2, 102, 'William', 'Edwards', 'wedwards1', 1);
-insert into tblUser (id, number, firstname, lastname, username, team_id) values (3, 103, 'Sharon', 'Howell', 'showell2', 1);
-insert into tblUser (id, number, firstname, lastname, username, team_id) values (4, 104, 'Roy', 'Rogers', 'rrogers3', 1);
-insert into tblUser (id, number, firstname, lastname, username, team_id) values (5, 105, 'Julia', 'Gonzales', 'jgonzales4', 1);
+insert into tblUser (id, number, firstname, lastname, username, team_id, password) values (1, 101, 'Kelly', 'Lewis', 'klewis0', 1, '1000:6a141086635a31296a28452b2b3fa407032c20e5886e14d7:21678a520d8f2e2abe044973568afd11118d073c1b9ca911');
+insert into tblUser (id, number, firstname, lastname, username, team_id, password) values (2, 102, 'William', 'Edwards', 'wedwards1', 1, '1000:6a141086635a31296a28452b2b3fa407032c20e5886e14d7:21678a520d8f2e2abe044973568afd11118d073c1b9ca911');
+insert into tblUser (id, number, firstname, lastname, username, team_id, password) values (3, 103, 'Sharon', 'Howell', 'showell2', 1, '1000:6a141086635a31296a28452b2b3fa407032c20e5886e14d7:21678a520d8f2e2abe044973568afd11118d073c1b9ca911');
+insert into tblUser (id, number, firstname, lastname, username, team_id, password) values (4, 104, 'Roy', 'Rogers', 'rrogers3', 1, '1000:6a141086635a31296a28452b2b3fa407032c20e5886e14d7:21678a520d8f2e2abe044973568afd11118d073c1b9ca911');
+insert into tblUser (id, number, firstname, lastname, username, team_id, password) values (5, 105, 'Julia', 'Gonzales', 'jgonzales4', 1, '1000:6a141086635a31296a28452b2b3fa407032c20e5886e14d7:21678a520d8f2e2abe044973568afd11118d073c1b9ca911');
 
 
 insert into tblWorkItem (id, number, description, status, issue_id) values (1, 101, 'Water the flowers', 'DONE', null);
